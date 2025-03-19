4f0438b9ff7bc72383a0c57dda2b54c9803b7338
soumen8890 <130748304+soumen8890@users.noreply.github.com>
2025-03-19 06:22:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "577678")
-    API_HASH = environ.get("API_HASH", "d2c6e01uuiuiouioiuiou0fc6d7a1be")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "70955...") 
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    API_ID = environ.get("API_ID", "20919625")
+    API_HASH = environ.get("API_HASH", "40168846bf06f4ff443f0f7a4182bf8d)
+    BOT_TOKEN = environ.get("BOT_TOKEN", "6569842591:AAFWT0-OAnqbaoHsczM7TQQ-NKFDho9nPoA") 
+    BOT_SESSION = environ.get("BOT_SESSION", "1BVtsOJABu4viqJmSLRAkLo35--Ea0dUV-K1hLsg1U7bq5xzdakKdHBGnSaCLEGczKlht99v7thsmThHdB55YdBn8diEnkQCncE1t1NdBOB1GaHyAnc2dsYNQL3BTKdJq6UAjiwlfShum8zY9NP4UL-loNHEhvkoRhE37kg4Dxvl-vg1qWE2RjSkdG7duC3lulx1eBDhGFXG1cnRaj3t6cCqfoyb9CYKPDHPoWkoIJH95AuJ7tjqTnZZBbKEXsqp-QRGmduknLjd-5lV_FW0S1zGXop4QM2rhp6L6p0DmNUYl3FYtCo-WxiCz-CEDEYv9fFbMrpGjJjL41FXS6fuKod09Jv7g0Bc=") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6964148334').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6233910543').split()]
 
 class temp(object): 
     lock = {}
@@ -15,4 +15,4 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-    
+   
